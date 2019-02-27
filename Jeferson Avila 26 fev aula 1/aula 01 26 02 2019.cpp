0e1d5d5df77f5ea02097cf99d0a9533c5b236ba3
590290 <47800548+590290@users.noreply.github.com>
2019-02-26 21:50:39 -0300

    Add files via upload

---
+++ Jeferson Avila 26 fev aula 1/aula 01 26 02 2019.cpp
@@ -0,0 +1,20 @@
+//Codigo de linguagem em C
+
+#include <stdio.h>
+
+// programa 'Le e imprime um valor´
+
+int main()
+{
+	//system("cls");
+	
+	int A=0;
+	
+	printf("informe um valor A inteiro:\n,A");
+	
+	scanf("%i",&A);
+	
+	printf(" o valor de A eh:%i\n",A);
+	
+	//system("pause")
+}
---
+++ Jeferson Avila 26 fev aula 1/aula 01 ex3  26 02 2019.cpp
@@ -0,0 +1,57 @@
+//Codigo de linguagem em C
+
+#include <stdio.h>
+
+// programa 'Menor que´
+
+int main()
+{
+
+	int A=0;
+	int B=0;
+	
+	printf ("Informe um numero inteiro A:\n\n");
+	scanf("%i",&A);
+	
+	printf ("Informe um numero interio B:\n\n");
+	scanf ("%i",&B);
+	
+	
+	if (A>B)
+	{
+		printf ("A e maior que B!\n");
+		
+		
+	}
+	
+	else
+	
+	
+		if (A<B)
+    {
+		
+			printf("A menor que B!\n");
+	
+    }
+		
+	
+	else
+	
+	{
+		
+		printf("A igual B!\n"); 
+		
+	}
+	
+	     return 0; 
+		
+}
+	
+		
+
+	
+	 
+	
+	
+	
+
